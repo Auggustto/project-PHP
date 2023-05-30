53378fd86b332a200b46ebdfd824fe01c2859701
Leonardo <leonardoaugusto1000a@gmail.com>
2023-05-29 21:30:49 -0300

    Inserindo funções

--- project/models/search.php
+++ project/models/search.php
@@ -1,40 +1,43 @@
 <?php
 
-class user_all{
-
+class user_all
+{
     private $pdo;
 
     // Conexão com o banco de dados OBS: Tudo que está dentro do function __construct(), será executado primeiro.
     public function __construct($dbname, $host, $user, $password)
     {
         try {
-            $this->pdo = new PDO("mysqli:dbname=".$dbname .
-            ":host=".$host,$user,$password); 
-        }    
+            $this->pdo = new PDO("mysqli:dbname=" .$dbname .";host=" .$host, $user, $password);
+        } 
         catch (PDOException $e) {
-            echo "Erro ao conectar com o banco de dados: ".$e->getMessage();
+            echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
             // Caso de erro para o codigo
             exit();
         }
         catch (Exception $e) {
-            echo "Erro generico: ".$e->getMessage();
+            echo "Erro generico: " . $e->getMessage();
             // Caso de erro para o codigo
             exit();
         }
     }
-
-    // Metodo para buscar todos os dados
-    public function search_values(){
         
+    // Metodo para buscar todos os dados
+    public function search_values()
+    {
+
         global $pdo;
 
         // Caso o db esteja vazio, a var res vai retornar um array vazio
         $res = array();
 
-        $select_all = $this -> $pdo ->prepare("SELECT * FROM users");
+        $select_all = $this->$pdo->prepare("SELECT * FROM users");
         $res = $select_all->fetchAll(PDO::FETCH_ASSOC);
         return $res;
-
     }
 }
+
+$teste = new user_all("users", "localhost", "root", "");
+
+echo $teste;
 ?>
--- project/view/home.php
+++ project/view/home.php
@@ -11,6 +11,7 @@ $u = new user_all("users", "localhost", "root", "");
 <!DOCTYPE html>
 <html lang="pt-br">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
