63fb7240d4dba6f646d91663cbbde32e53d14181
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-02 11:33:36 -0300

    Composer

--- app/controller/Homecontroller.php
+++ app/controller/Homecontroller.php
@@ -1,18 +1,24 @@
 <?php
 
-class HomeController 
+//use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+
+class HomeController
 {
     // Exibindo a mengasem HOME
     public function index()
     {
-        try 
-        {
+        try {
+            //$loader = new FilesystemLoader('/app/view/home');
+            //$twig = new \Twig\Environment($loader);
+
             // Chamando o metodo search_db
             $dataAll = search::select_all();
             var_dump($dataAll);
-        } 
-        catch (Exception $e)
-        {
+            
+        } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require_once 'app/core/core.php';
 require_once 'app/controller/Homecontroller.php';
 require_once 'app/controller/ErroController.php';
 require_once 'app/lib/database/conecction.php';
-require_once 'vendor/autoload.php';
+//require_once 'vendor/autoload.php';
+
 require_once 'app/models/search_db.php';
 
 $template = file_get_contents('app/view/home.html');
@@ -23,5 +24,5 @@ ob_start();
 ob_end_clean();
 
 // Carregando template que tem o conteudo GTML
-$tplPronto = str_replace('{{area_dinamica}}', $saida, $template);
+$tplPronto = str_replace('area_dinamica', $saida, $template);
 echo $tplPronto;
